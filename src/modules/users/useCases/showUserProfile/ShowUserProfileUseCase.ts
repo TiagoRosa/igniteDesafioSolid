e57b60f0ad42e18446a5991377aa2828c49f649b
Tiago Rosa <tiago.tarinfo@gmail.com>
2022-01-03 20:02:29 -0300

    desafio solid

--- src/modules/users/useCases/showUserProfile/ShowUserProfileUseCase.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileUseCase.ts
@@ -9,7 +9,12 @@ class ShowUserProfileUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id.toString());
+    if (!user) {
+      throw new Error("User is not exists");
+    }
+
+    return user;
   }
 }
 
